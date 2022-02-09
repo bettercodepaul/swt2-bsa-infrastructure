7f6add402dfb8d571439b2ad995e0a28f0112837
Andre Lehnert <andre.lehnert@bcxp.de>
2022-02-09 09:10:56 +0100

    Rechte zur Ausführung der Skripte geändert

--- script/update.sh
+++ script/update.sh
@@ -5,17 +5,10 @@
 set -e
 cd "$(dirname "$0")/.."
 
-# Load configurationBE
+# Load configuration
 source script/_config
 
 script/bootstrap.sh
-
-
-#echo "==> Pulling frontend..."
-#git -C $FRONTEND_DIRECTORY pull 
-
-#echo "==> Pulling backend..."
-#git -C $BACKEND_DIRECTORY pull 
 
 echo "==> Building frontend..."
 script/step/build-angular-application $FRONTEND_DIRECTORY
@@ -24,4 +17,3 @@ script/step/provide-angular-dist $FRONTEND_DIRECTORY $DOCKER_FRONTEND_DIRECTORY
 echo "==> Building backend..."
 script/step/build-maven-application $BACKEND_DIRECTORY
 script/step/provide-jar-file $BACKEND_PROJECT_DIRECTORY $BACKEND_PROJECT_JAR_FILE $DOCKER_BACKEND_DIRECTORY
-
