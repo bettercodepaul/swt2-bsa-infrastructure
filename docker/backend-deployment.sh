2b53f1984dedf7f52c72f37282f1b851a6c9530b
Konisonis <35600104+Konisonis@users.noreply.github.com>
2023-03-15 15:32:03 +0100

    Update backend-deployment.sh
    changed docker excellent nexus to bcxp nexus

--- docker/backend-deployment.sh
+++ docker/backend-deployment.sh
@@ -2,7 +2,7 @@ if [ -z ${1} ]; then echo 'ERROR: PASS BACKEND_VERSION AS FIRST PARAMETER'; exit
                                                                                                                                                
 echo "---- start deployment ----"                                                                                                              
                                                                                                                                                
-sudo docker login -u username -p passwort nexus.cs.exxcellent.de:8484                                                                   
+sudo docker login -u username -p passwort nexus.bcxp.de:8484                                                                   
 sudo docker-compose -f docker-compose-tag-based.yml stop backend                                                                               
 BACKEND_VERSION=$1                                                                                                                             
 echo "deploy new Backend-Version ${BACKEND_VERSION}"                                                                                           
